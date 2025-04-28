5eabd1efe1d0e4fff356bcedce911eb87785857d
Leonardo Grando <lgrando@prof.educacao.sp.gov.br>
2025-04-27 23:21:03 -0300

    das

---
+++ dashboardvue/dashboard-app/src/components/Footer.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <footer>
+    
+    <p>&copy; 2024 WebCraft Solutions. Todos os direitos reservados.</p>
+    
+    </footer>
+    
+    </template>
+    
+    
+    <script>
+    
+    export default {
+    
+    name: 'AppFooter'
+    
+    }
+    
+    </script>
+    
+    
+    <style scoped>
+    
+    footer {
+    
+    background-color: #333;
+    
+    color: white;
+    
+    padding: 10px;
+    
+    text-align: center;
+    
+    position: fixed;
+    
+    bottom: 0;
+    
+    width: 100%;
+    
+    }
+    
+    </style>
---
+++ dashboardvue/dashboard-app/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+
+    <header>
+    
+    <h1>Dashboard WebCraft Solutions</h1>
+    
+    </header>
+    
+    </template>
+    
+    
+    <script>
+    
+    export default {
+    
+    name: 'AppHeader'
+    
+    }
+    
+    </script>
+    
+    
+    <style scoped>
+    
+    header {
+    
+    background-color: #3b5998;
+    
+    color: white;
+    
+    padding: 10px;
+    
+    text-align: center;
+    
+    }
+    
+    </style>
