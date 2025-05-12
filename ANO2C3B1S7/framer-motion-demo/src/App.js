658001eabe82f0d02e9f70b7c62439b606700f24
Leonardo Grando <lgrando@outlook.com>
2025-05-11 22:20:42 -0300

    leoee

--- ANO2C3B1S7/framer-motion-demo/src/App.js
+++ ANO2C3B1S7/framer-motion-demo/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import { motion } from 'framer-motion';
+
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [isAnimating, setIsAnimating] = useState(false);
+
+
+return (
+
+<div className="App">
+
+<motion.div
+
+id="box"
+
+animate={{
+
+x: isAnimating ? 300 : 0,
+
+}}
+
+transition={{ duration: 2 }}
+
+style={{
+
+width: 100,
+
+height: 100,
+
+backgroundColor: 'red',
+
+marginTop: 50,
+
+}}
+
+/>
+
+<button
+
+id="animateButton"
+
+onClick={() => setIsAnimating(!isAnimating)}
+
+style={{ marginTop: 20 }}
+
+>
+
+Iniciar Animação
+
+</button>
+
+</div>
+
+);
+
 }
+
 
 export default App;
