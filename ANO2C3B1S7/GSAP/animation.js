79fd7e75635cca1d6102d6614ad9d444c992f2cb
Leonardo Grando <lgrando@outlook.com>
2025-05-11 22:08:22 -0300

    tleo

---
+++ ANO2C3B1S7/GSAP/animation.js
@@ -0,0 +1,33 @@
+// JavaScript puro
+
+document.getElementById('animateButton').addEventListener('click', function() {
+
+const box = document.getElementById('box');
+
+let position = 0;
+
+const interval = setInterval(function() {
+
+if (position >= 300) {
+
+clearInterval(interval);
+
+} else {
+
+position++;
+
+box.style.transform = `translateX(${position}px)`;
+
+}
+
+}, 5);
+
+});
+
+// Animação usando GSAP
+
+document.getElementById('animateButton').addEventListener('click', function() {
+
+gsap.to("#box", { duration: 2, x: 300 });
+
+});
