fb64ed066d64aa35b7011ae1b57e7bbb156fbe07
Leonardo Grando <lgrando@prof.educacao.sp.gov.br>
2025-06-01 20:06:59 -0300

    dasd

--- ANO2C3B2S10/techgear-cart/src/redux/store.js
+++ ANO2C3B2S10/techgear-cart/src/redux/store.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import cartReducer from './cartReducer';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import logger from 'redux-logger';
 
-// Crie a Store e associe o reducer
-const store = createStore(cartReducer);
+// Crie a Store e associe o reducer com redux-logger e Redux DevTools
+const store = createStore(cartReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
